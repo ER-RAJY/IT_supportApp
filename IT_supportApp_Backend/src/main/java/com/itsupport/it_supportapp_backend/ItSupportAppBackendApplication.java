11350cf6a3bcdf5ffdcf37bd6f587ca8bcec3d1b
ER-RAJY <noureddineerajy@gmail.com>
2024-08-05 16:25:09 +0100

    creation du projet

---
+++ IT_supportApp_Backend/src/main/java/com/itsupport/it_supportapp_backend/ItSupportAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itsupport.it_supportapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSupportAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSupportAppBackendApplication.class, args);
+	}
+
+}
