c283037be8441fb20e55d441603f236a8325319f
ER-RAJY <noureddineerajy@gmail.com>
2024-08-07 15:29:19 +0100

    des update sue les relation

--- IT-support-App/src/main/java/com/ITsupport/IT/support/App/controller/EquuipmentsCotroller.java
+++ IT-support-App/src/main/java/com/ITsupport/IT/support/App/controller/EquuipmentsCotroller.java
@@ -5,17 +5,25 @@ import com.ITsupport.IT.support.App.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/equipment")
+@RequestMapping("/equipment")
 public class EquuipmentsCotroller {
 
      @Autowired
     EquipmentService equipmentService;
 
+     @GetMapping("/all")
+     public List<Equipment> getEquipment(){
+         return equipmentService.getAllEquipements();
+     }
+
      @PostMapping("/add")
      public Equipment addEquipment(@RequestBody Equipment equipment){
          return equipmentService.addEquipment(equipment);
      }
+
 
      @DeleteMapping("/delete/{id}")
     public void deleteEquipment(@PathVariable Long id){
